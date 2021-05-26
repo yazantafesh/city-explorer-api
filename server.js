3e8a64e5f059eac2fc5b0d5f9fddf7c94b33623e
yazantafesh <yazantafesh1@gmail.com>
2021-05-26 20:08:07 +0300

    done backend

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 require('dotenv').config();
 const express = require('express');
-const weatherData = require('./data/weather.json');
+// const weatherData = require('./data/weather.json');
 const cors = require('cors');
+const axios = require('axios');
 
 const server = express();
 server.use(cors());
@@ -14,51 +15,73 @@ server.listen(PORT, () => {
     console.log(`Listening to PORT ${PORT}`);
 })
 
-server.get('/weather', (req, res) => {
-    let weatherNameData = req.query.city;
-    let weatherLatData = req.query.lat;
-    let weatherLonData = req.query.lon;
+server.get('/weather', weatherHandler);
 
-    let allWeatherData = weatherData.find(element => {
-        if (weatherNameData == element.city_name.toLowerCase() && weatherLatData == element.lat && weatherLonData == element.lon) {
-            return element;
-        }
-    })
+server.get('/movie', movieHandler)
 
-    try {
+function weatherHandler(req, res) {
 
-        let forecastArray = [];
-        let date;
-        let description;
-        let forecastData;
+    let weatherQuery = req.query.city;
+    let key = process.env.WEATHER_API_KEY;
+    // let weatherUrl = 'https://api.weatherbit.io/v2.0/forecast/daily?city=london&key=9342e4a8d5ae4e59aaf275f75ed61936'
+    let weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${weatherQuery}&key=${key}`;
 
-        for (let i = 0; i < allWeatherData.data.length; i++) {
+    axios
+        .get(weatherUrl)
+        .then(result => {
+            const weatherArray = result.data.data.map(weatherItem => {
+                return new Forecast(weatherItem);
+            })
+            res.send(weatherArray);
+        })
+        .catch(err => {
 
-            date = allWeatherData.data[i].valid_date;
+            res.status(500).send(`Weather data for this city is not found ${err}`);
+        })
+}
 
-            description = `Low of ${allWeatherData.data[i].min_temp}, high of ${allWeatherData.data[i].max_temp} with ${allWeatherData.data[i].weather.description}`;
+function movieHandler(req, res) {
 
-            forecastData = new Forecast(date, description);
+    let movieQuery = req.query.city;
+    let key = process.env.MOVIE_API_KEY;
+    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${movieQuery}`;
+    // let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=506d0b8623be28cadfcac3e1d0be27b5&query=seattle`;
 
-            forecastArray.push(forecastData);
-        }
+    axios
+        .get(movieUrl)
+        .then(result => {
+            const movieArray = result.data.results.map(movieItem => {
+                return new Movie(movieItem);
+            })
+            res.send(movieArray);
+        })
+        .catch(err => {
 
-        res.send(forecastArray);
+            res.status(500).send(`Movie data related to this city is not found ${err}`);
+        })
+}
 
-    } catch {
-        res.status(500).send('Weather data for this city is not found');
-    }
-})
 
 server.get('*', (req, res) => {
     res.send('not found');
 })
 
 class Forecast {
-    constructor(date, description) {
-        this.date = date;
-        this.description = description;
+    constructor(item) {
+        this.date = item.valid_date;
+        this.description = `Low of ${item.min_temp}, high of ${item.max_temp} with ${item.weather.description}`;
     }
+}
 
-
+class Movie {
+    constructor(item) {
+        this.title = item.original_title;
+        this.overview = item.overview;
+        this.average_votes = item.vote_average;
+        this.total_votes = item.vote_count;
+        this.image_url = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
+        this.popularity = item.popularity;
+        this.released_on = item.release_date;
+        
+    }
 }
